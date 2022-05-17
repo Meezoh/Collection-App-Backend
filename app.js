0ce965ed093b644a8505f722ebfdd3c8027f3c2f
Abdulhameed Hameed <hameed.babba@gmail.com>
2022-05-17 14:26:57 +0200

    make it better

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+});
 
 // routes
 app.use('/api/login', login);
