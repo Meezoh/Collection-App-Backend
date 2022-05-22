da3404595e4c89bee9693df5349a8694386f3527
Abdulhameed Hameed <hameed.babba@gmail.com>
2022-05-22 23:52:26 +0200

    make it better

--- routes/items.js
+++ routes/items.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.route('/').delete(deleteItem);
 router.route('/create/:kollectionId').post(createItem);
-router.route('/kollection/:kollectionId').get(kollectionItems);
+router.route('/:kollectionId').get(kollectionItems);
 router.route('/:id').patch(updateItem);
 router.route('/:id/likes').put(like);
 router.route('/:id/comments').put(comment);
